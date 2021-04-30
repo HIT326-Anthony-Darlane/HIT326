3172469f3f301f89a943e13d136362d01c6d0d2f
Cameron <s332437@students.cdu.edu.au>
2021-04-30 13:20:46 +0930

    testttttttt

--- create.sql
+++ create.sql
@@ -1,1 +1,23 @@
-/*test*/
+use article_db;
+
+drop table if exists author;
+
+
+create table author(
+  author_id int(50) NOT NULL AUTO_INCREMENT,
+  firstname varchar(40) not null,
+  lastname varchar(40) not null,
+  primary key (author_id)
+);
+
+/*create table article(
+  article_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  title varchar(100) not null,
+  published_at  DEFAULT(datetime('now','localtime')),
+  updated_at
+);
+
+
+
+--drop table if exists article;--
+*/
---
+++ load.sql
@@ -0,0 +1,2 @@
+insert into author (firstname,lastname) values ("Jon","Smith");
+insert into author (firstname,lastname) values ("Jane","Doe");
